f3f1aa11a4d8cb3e89da6ee83f8244975d533769
Tobin C. Harding <me@tobin.cc>
2023-02-10 13:18:32 +1100

    Add basic test script

---
+++ contrib/test.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+set -ex
+
+FEATURES=""
+
+cargo --version
+rustc --version
+
+# Make all cargo invocations verbose
+export CARGO_TERM_VERBOSE=true
+
+# Defaults / sanity checks
+cargo build
+cargo test
+
+if [ "$DO_LINT" = true ]
+then
+    cargo clippy --features="$FEATURES" --all-targets
+fi
+
+if [ "$DO_FEATURE_MATRIX" = true ]; then
+    # All features
+    cargo build --features="$FEATURES"
+    cargo test --features="$FEATURES"
+    # Single features
+    for feature in ${FEATURES}
+    do
+        cargo build --features="$feature"
+        cargo test --features="$feature"
+		# All combos of two features
+		for featuretwo in ${FEATURES}; do
+			cargo build --features="$feature $featuretwo"
+			cargo test --features="$feature $featuretwo"
+		done
+    done
+fi
+
+# Build the docs if told to (this only works with the nightly toolchain)
+if [ "$DO_DOCS" = true ]; then
+    RUSTDOCFLAGS="--cfg docsrs" cargo doc --features="$FEATURES"
+fi
